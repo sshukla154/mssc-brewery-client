6fcd8806b03c4feb2bd7cf8a09506b293b84ba28
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2020-11-23 23:45:55 +0530

    Validation - Using Javax Validation Annotations

--- src/main/java/frontier/learning/msscbreweryclient/web/model/BeerDTO.java
+++ src/main/java/frontier/learning/msscbreweryclient/web/model/BeerDTO.java
@@ -2,6 +2,10 @@ package frontier.learning.msscbreweryclient.web.model;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +17,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDTO {
 
+	@Null
 	private UUID id;
+
+	@NotNull
 	private String beerName;
+
+	@NotNull
 	private BeerStyleEnum beerStyle;
+
+	@Positive
 	private Long upc;
 }
