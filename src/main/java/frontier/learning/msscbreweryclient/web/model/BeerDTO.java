8ca805440000dca93145eda61551c3abe2727884
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2020-11-24 08:21:14 +0530

    Revert "Validation - Using Javax Validation Annotations"
    This reverts commit 6fcd8806b03c4feb2bd7cf8a09506b293b84ba28.

--- src/main/java/frontier/learning/msscbreweryclient/web/model/BeerDTO.java
+++ src/main/java/frontier/learning/msscbreweryclient/web/model/BeerDTO.java
@@ -2,10 +2,6 @@ package frontier.learning.msscbreweryclient.web.model;
 
 import java.util.UUID;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,15 +13,8 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDTO {
 
-	@Null
 	private UUID id;
-
-	@NotNull
 	private String beerName;
-
-	@NotNull
 	private BeerStyleEnum beerStyle;
-
-	@Positive
 	private Long upc;
 }
